f40a7e07b82d2e6fcca449812a38c2fc961fe73e
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-03-03 16:03:10 -0800

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql');
 
-const connection = mysql.createPool({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'myPass',
-  database : 'burgers_db'
-});
+// db connection
+let connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createPool(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'myPass',
+    connectionLimit: 20,
+    database : 'burgers_db'
+  });
+}
 
 module.exports = connection;
